e2c4f7bf3584d607a3c44175e52b21f1a1ebb0f1
codesthenos <codesthenos@gmail.com>
2024-10-18 00:21:46 +0200

    Tengo que arreglar algo de los maximos de ancho y alto, pero por ahora el footer y el header han entrado bien

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import './styles/common/normalize.css'
+import './styles/common/variables.css'
+import './styles/common/body.css'
+
+import Banner from "./components/Banner.tsx"
+import Contact from "./components/Contact.tsx"
+import Description from "./components/Description.tsx"
+import Footer from "./components/Footer.tsx"
+import Header from "./components/Header.tsx"
+import Skills from "./components/Skills.tsx"
+
 function App () {
   return (
     <>
---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import '../styles/index/header-burger-menu.css'
+
+function Header () {
+  return (
+    <header className="body-header">
+      <h1 className="sr-only">CODESTHENOS PORTFOLIO</h1>
+      <img
+        className="dark-mode"
+        id="white-header-logo"
+        src="/strength-gains-white-icon.png"
+        alt="Strength-gains branding logo in white: two people forming a headstand pyramid."
+      />
+
+      <img
+        className="light-mode"
+        id="dark-header-logo"
+        src="/strength-gains-dark-icon.png"
+        alt="Strength-gains branding logo in dark: two people forming a headstand pyramid."
+      />
+
+      <nav aria-label="Main web navigation" role="navigation">
+        <input type="checkbox" id="navigation-menu" />
+        <label
+          htmlFor="navigation-menu"
+          aria-label="Main menu"
+          role="button"
+        ></label>
+        <ul>
+          <li>
+            <a className="link" href="#skills">Skills</a>
+          </li>
+
+          <li>
+            <a className="link" href="#projects">Projects</a>
+          </li>
+
+          <li>
+            <a className="link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header
