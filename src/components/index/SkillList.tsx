1deac2a4429007c38fa940db8fa2af006c7c17fb
codesthenos <codesthenos@gmail.com>
2024-10-20 01:50:22 +0200

    Componetizo la lsita de skills, y ademas creo la constante skills que es una lista de objetos, que uso para que el return del componente sea mucho ams corto y menos repetitivo, aunque al final, soy repetitivo en la estructura de datos, los datos hay que meterlos de alguna manera

---
+++ src/components/index/SkillList.tsx
@@ -0,0 +1,26 @@
+import { SKILLS } from '../../consts.ts'
+
+function SkillList () {
+  return (
+    <ul>
+      {
+        SKILLS.map((skill, index) => (
+          <li key={index}>
+            <h4>{skill.skillTitle}</h4>
+            <div className="total-progress">
+              <div
+                className="current-progress"
+                role="progressbar"
+                aria-valuemin={skill.valueMin}
+                aria-valuemax={skill.valueMax}
+                aria-valuenow={skill.valueNow}
+                id={skill.id}
+              ></div>
+            </div>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+export default SkillList
---
+++ src/consts.ts
@@ -0,0 +1,44 @@
+export const SKILLS = [
+  {
+    skillTitle: 'Git - GitHub',
+    valueMin: 0,
+    valueNow: 50,
+    valueMax: 100,
+    id: 'git-github-progress'
+  },
+  {
+    skillTitle: 'HTML',
+    valueMin: 0,
+    valueNow: 65,
+    valueMax: 100,
+    id: 'html-progress'
+  },
+  {
+    skillTitle: 'CSS',
+    valueMin: 0,
+    valueNow: 55,
+    valueMax: 100,
+    id: 'css-progress'
+  },
+  {
+    skillTitle: 'Javascript',
+    valueMin: 0,
+    valueNow: 80,
+    valueMax: 100,
+    id: 'javascript-progress'
+  },
+  {
+    skillTitle: 'NodeJS',
+    valueMin: 0,
+    valueNow: 25,
+    valueMax: 100,
+    id: 'nodejs-progress'
+  },
+  {
+    skillTitle: 'ReactJS',
+    valueMin: 0,
+    valueNow: 35,
+    valueMax: 100,
+    id: 'reactjs-progress'
+  }
+]
