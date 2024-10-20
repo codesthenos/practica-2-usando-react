bd7bdf14891accb8c58a31ad81d5a8b2e609af90
codesthenos <codesthenos@gmail.com>
2024-10-20 02:18:59 +0200

    Para terminar, incluyo un boton para cambiar el theme, muy facilmente y facilmente aditable, introduce los estilos metiendo una clase o quitandosela, y esos estilos estan en el index.css en un selector que tiene prioridad sobre el resto

--- src/components/index/Header.tsx
+++ src/components/index/Header.tsx
@@ -1,6 +1,12 @@
 import '../../styles/index/header-burger-menu.css'
 
 function Header () {
+  const divRoot = document.getElementById('root')
+
+  const handleClick = () => {
+    if (!divRoot) return
+    divRoot.classList.toggle('light-mode-button-class')
+  }
   return (
     <header className="indexHeader">
       <h1 className="sr-only">CODESTHENOS PORTFOLIO</h1>
@@ -17,6 +23,8 @@ function Header () {
         src={`${import.meta.env.BASE_URL}/strength-gains-dark-icon.png`}
         alt="Strength-gains branding logo in dark: two people forming a headstand pyramid."
       />
+
+      <button className='button' onClick={handleClick}>Switch Theme</button>
 
       <nav aria-label="Main web navigation" role="navigation">
         <input type="checkbox" id="navigation-menu" />
